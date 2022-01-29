f99c1b78e47c3dc2f5d8ea7d3b5bc9ab7efb3a61
Viktor Holovin <holovin@mail.com>
2022-01-29 22:47:18 +0100

    Testing messages

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -13,4 +26,16 @@ async def root():
             "id": 1,
             "message": "Testing second message.",
         },
+        {
+            "id": 2,
+            "message": "Testing third message.",
+        },
+        {
+            "id": 3,
+            "message": "Testing fourth message.",
+        },
     ]
+
+@app.get("/{service_id}")
+async def read_service(service_id):
+    return {"service_id": service_id}
